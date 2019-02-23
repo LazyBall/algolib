453c7396c5d307518081b32c722c98817015c8b1
Alexander <informery4@gmail.com>
2019-02-24 00:30:27 +0300

    Added Lab2

---
+++ Lab2(Trees)/Trees/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class BinaryTree<TKey, TValue>
+        where TKey:IComparable<TKey>
+    {
+        public int Count { get; private set; }
+        private Node<TKey,TValue> Root { get; set; }
+
+
+        public void Add(TKey key, TValue value)
+        {
+            var current = Root;
+            while (current != null)
+            {
+                if (current.Key.CompareTo(key) > 0)
+                {
+                    current = current.Left;
+                }
+                else if(current.Key.CompareTo(key)<0)
+                {
+                    current = current.Right;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            current = new Node<TKey, TValue>(key, value);
+            Count++;
+        }
+
+
+
+    }
+}
