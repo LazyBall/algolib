b201a5e1dd9ceda6c2cffff49098cbcd5eb1a11a
Alexander <informery4@gmail.com>
2019-03-07 02:06:30 +0300

    Added ConsoleApp

---
+++ Lab2(Trees)/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Trees;
+using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = 1000000, startDel = 500000, stopDel = 700000;
+            var worker = new Worker();
+            var array = worker.CreateRandomArray(length);
+            var watch = new Stopwatch();
+            watch.Start();
+            worker.TestTree<BinarySearchTree<int, int>>(array, startDel, stopDel);
+            watch.Stop();
+            Console.WriteLine($"BinarySearchTree \n Time: {watch.ElapsedMilliseconds} ms.");
+            Console.WriteLine();
+            watch.Restart();
+            worker.TestTree<SortedDictionary<int, int>>(array, startDel, stopDel);
+            watch.Stop();
+            Console.WriteLine($"SortDictionary \n Time: {watch.ElapsedMilliseconds} ms.");
+        }
+    }
+}
---
+++ Lab2(Trees)/ConsoleApp/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Worker
+    {
+        public int[] CreateRandomArray(int length)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+
+        public void TestTree<T>(int[] array, int startDelete, int stopDelete)
+            where T : IDictionary<int, int>, new()
+        {
+            var dict = new T();
+            AddElements(dict, array);
+            RemoveElements(dict, array, startDelete, stopDelete);
+            FindElements(dict, array);
+        }
+
+        private void AddElements(IDictionary<int, int> dict, int[] array)
+        {
+            foreach (var elem in array)
+            {
+                if (!dict.ContainsKey(elem))
+                {
+                    dict.Add(elem, elem);
+                }
+            }
+        }
+
+        private void RemoveElements(IDictionary<int, int> dict, int[] array, int startDelete,
+            int stopDelete)
+        {
+            for (int i = startDelete; i < stopDelete; i++)
+            {
+                dict.Remove(array[i]);
+            }
+        }
+
+        private void FindElements(IDictionary<int, int> dict, int[] array)
+        {
+            foreach(var element in array)
+            {
+                dict.ContainsKey(element);
+            }
+        }
+    }
+}
