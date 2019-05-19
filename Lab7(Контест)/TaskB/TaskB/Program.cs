db7017163d3f89e880e44c0702366b762ec43bde
Alexander <informery4@gmail.com>
2019-05-19 17:14:44 +0300

    Added Lab7(Contest)

---
+++ Lab7(Контест)/TaskB/TaskB/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TaskB
+{
+    class Program
+    {
+        static void Main()
+        {
+            int start;
+            int[] cities;
+            var graph = ReadFromFile("input.txt", out start, out cities);
+            var distances = DijkstrasAlgorithm.FindShortestPaths(graph, start);
+            var cityTimePairs = new KeyValuePair<int, int>[cities.Length];
+
+            for (int i = 0; i < cities.Length; i++)
+            {
+                var city = cities[i];
+                cityTimePairs[i] = new KeyValuePair<int, int>(city, distances[city]);
+            }
+
+            Array.Sort(cityTimePairs, delegate (KeyValuePair<int, int> cityTimePair1,
+                 KeyValuePair<int, int> cityTimePair2)
+            {
+                var comparisonResult = cityTimePair1.Value.CompareTo(cityTimePair2.Value);
+                if (comparisonResult == 0)
+                {
+                    comparisonResult = cityTimePair1.Key.CompareTo(cityTimePair2.Key);
+                }
+                return comparisonResult;
+            });
+            WriteToFile("output.txt", cityTimePairs);
+        }
+
+
+        static Graph<int> ReadFromFile(string fileName,
+            out int start, out int[] cities)
+        {
+            using (var inputFile = new StreamReader(fileName))
+            {
+                var sep = new char[] { ' ' };
+                var option = StringSplitOptions.RemoveEmptyEntries;
+                var str = inputFile.ReadLine().Split(sep, option);
+                var graph = new Graph<int>(int.Parse(str[0]));
+                int edgesCount = int.Parse(str[1]);
+                cities = new int[int.Parse(str[2])];
+                start = int.Parse(str[3]) - 1;
+                str = inputFile.ReadLine().Split(sep, option);
+
+                for (int i = 0; i < cities.Length; i++)
+                {
+                    cities[i] = int.Parse(str[i]) - 1;
+                }
+
+                for (int i = 0; i < edgesCount; i++)
+                {
+                    str = inputFile.ReadLine().Split(sep, option);
+                    int leftVertex = int.Parse(str[0]) - 1,
+                        rightVertex = int.Parse(str[1]) - 1;
+                    int weight = int.Parse(str[2]);
+
+                    graph[leftVertex].Add(new KeyValuePair<int, int>(rightVertex, weight));
+                    graph[rightVertex].Add(new KeyValuePair<int, int>(leftVertex, weight));
+                }
+
+                return graph;
+            }
+        }
+
+        static void WriteToFile(string fileName, IEnumerable<KeyValuePair<int, int>> cityTimePairs)
+        {
+            using (var outputFile = new StreamWriter(fileName))
+            {
+                foreach (var cityTimePair in cityTimePairs)
+                {
+                    outputFile.WriteLine("{0} {1}", cityTimePair.Key + 1, cityTimePair.Value);
+                }
+            }
+        }
+    }
+
+    class Graph<TWeight>
+    {
+        private List<KeyValuePair<int, TWeight>>[] _adjacencyList;
+        public int NumberVertices { get; private set; }
+        public List<KeyValuePair<int, TWeight>> this[int vertex]
+        {
+            get
+            {
+                return _adjacencyList[vertex];
+            }
+        }
+
+        public Graph(int numberVertices)
+        {
+            this.NumberVertices = numberVertices;
+            _adjacencyList = new List<KeyValuePair<int, TWeight>>[this.NumberVertices];
+
+            for (int i = 0; i < _adjacencyList.Length; i++)
+            {
+                _adjacencyList[i] = new List<KeyValuePair<int, TWeight>>();
+            }
+        }
+    }
+
+    static class DijkstrasAlgorithm
+    {
+        public static int[] FindShortestPaths(Graph<int> graph, int start)
+        {
+            var distances = new int[graph.NumberVertices];
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = int.MaxValue;
+            }
+
+            distances[start] = 0;
+
+            foreach (var edge in graph[start])
+            {
+                var newDistance = edge.Value;
+                if (newDistance < distances[edge.Key])
+                {
+                    distances[edge.Key] = newDistance;
+                }
+            }
+
+            var visited = new HashSet<int>
+            {
+                start
+            };
+
+            while (visited.Count < graph.NumberVertices)
+            {
+                int currentVertex = 0;
+                int minDistance = int.MaxValue;
+
+                for (int i = 0; i < distances.Length; i++)
+                {
+                    if (!visited.Contains(i))
+                    {
+                        if (distances[i] < minDistance)
+                        {
+                            currentVertex = i;
+                            minDistance = distances[i];
+                        }
+                    }
+                }
+
+                if (minDistance < int.MaxValue)
+                {
+                    visited.Add(currentVertex);
+
+                    foreach (var edge in graph[currentVertex])
+                    {
+                        if (!visited.Contains(edge.Key))
+                        {
+                            int newDistance = distances[currentVertex] + edge.Value;
+                            if (newDistance < distances[edge.Key])
+                            {
+                                distances[edge.Key] = newDistance;
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return distances;
+        }
+    }
+}
---
+++ Lab7(Контест)/TaskC/TaskC/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TaskC
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputData = ReadFromFile("input.txt");
+            var pair = FloydWarshallAlgorithm.FindShortestPaths(inputData.Edges,
+                inputData.NumberVertices);
+            inputData.Edges = null;
+            var distances = pair.Key;
+            var next = pair.Value;
+
+            int start = 0, end = 0, maxDist = -1;
+            for (int i = 1; i < inputData.NumberVertices; i++)
+            {
+                for (int j = i + 1; j < inputData.NumberVertices; j++)
+                {
+                    if (distances[i, j] != int.MaxValue && distances[i, j] > maxDist)
+                    {
+                        start = i;
+                        end = j;
+                        maxDist = distances[i, j];
+                    }
+                }
+            }
+
+            var path = new List<int>();
+            if (maxDist == -1)
+            {
+                maxDist = 0;
+            }
+            else
+            {
+                path = FloydWarshallAlgorithm.GetShortestPath(start, end, next);
+            }
+            WriteToFile("output.txt", maxDist, path);
+
+        }
+
+        static InputData ReadFromFile(string fileName)
+        {
+            var data = new InputData();
+            using (var inputFile = new StreamReader(fileName))
+            {
+                var sep = new char[] { ' ' };
+                var option = StringSplitOptions.RemoveEmptyEntries;
+                var str = inputFile.ReadLine().Split(sep, option);
+                data.NumberVertices = int.Parse(str[0]) + 1;
+                int countEdges = int.Parse(str[1]) * 2;
+                var edges = new int[countEdges][];
+
+                for (int i = 0; i < countEdges; i++)
+                {
+                    str = inputFile.ReadLine().Split(sep, option);
+                    edges[i] = new int[3];
+                    int left = int.Parse(str[0]), right = int.Parse(str[1]),
+                        weight = int.Parse(str[2]);
+                    edges[i][0] = left;
+                    edges[i][1] = right;
+                    edges[i][2] = weight;
+                    i++;
+                    edges[i] = new int[3];
+                    edges[i][0] = right;
+                    edges[i][1] = left;
+                    edges[i][2] = weight;
+                }
+
+                data.Edges = edges;
+            }
+
+            return data;
+        }
+
+        static void WriteToFile(string fileName, int distance, List<int> path)
+        {
+            using (var outputFile = new StreamWriter(fileName))
+            {
+                outputFile.WriteLine(distance);
+                foreach (var item in path)
+                {
+                    outputFile.Write("{0} ", item);
+                }
+            }
+        }
+
+    }
+
+    class InputData
+    {
+        public int NumberVertices { get; set; }
+        public int[][] Edges { get; set; }
+    }
+
+    static class FloydWarshallAlgorithm
+    {
+        private static KeyValuePair<int[,], int[,]> FindShortestPaths(int[,] adjacencyMatrix)
+        {
+            int n = adjacencyMatrix.GetLength(0);
+            var distance = adjacencyMatrix;
+            var next = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (distance[i, j] == int.MaxValue)
+                    {
+                        next[i, j] = -1;
+                    }
+                    else
+                    {
+                        next[i, j] = j;
+                    }
+                }
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        var newDist = (long)distance[i, k] + distance[k, j];
+                        if (newDist < distance[i, j])
+                        {
+                            distance[i, j] = (int)newDist;
+                            next[i, j] = k;
+                        }
+                    }
+                }
+            }
+
+            return new KeyValuePair<int[,], int[,]>(distance, next);
+        }
+
+        public static KeyValuePair<int[,], int[,]> FindShortestPaths(int[][] edges, int numberVertices)
+        {
+            return FindShortestPaths(CreateAdjacencyMatrix(edges, numberVertices));
+        }
+
+        private static int[,] CreateAdjacencyMatrix(int[][] edges, int numberVertices)
+        {
+            var matrix = new int[numberVertices, numberVertices];
+
+            for (int i = 0; i < numberVertices; i++)
+            {
+                for (int j = 0; j < numberVertices; j++)
+                {
+                    matrix[i, j] = int.MaxValue;
+                }
+                matrix[i, i] = 0;
+            }
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                int row = edges[i][0], column = edges[i][1];
+                matrix[row, column] = edges[i][2];
+            }
+
+            return matrix;
+        }
+
+        public static List<int> GetShortestPath(int start, int end, int[,] next)
+        {
+            var path = new List<int>();
+            if (next[start, end] != -1)
+            {
+                var current = start;
+                while (current != end)
+                {
+                    path.Add(current);
+                    current = next[current, end];
+                }
+                path.Add(end);
+            }
+            return path;
+        }
+    }
+}
